a62b64306666fa8b781dad188691958c74b1fcc3
passpos <paiap@outlook.com>
2020-06-01 01:54:35 +0800

    update Signed-off-by:passpos <paiap@outlook.com>

---
+++ src/Json/Transformer/JsonData.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+namespace SfUtils\Json\Transformer;
+
+/**
+ * Interconversion between JSON and Array.
+ *
+ * @author passpos <paiap@outlook.com>
+ */
+class JsonData {
+
+    /**
+     * Json to Array.
+     * @param string $path Get json file path.
+     * @return array|null
+     */
+    public function getArrayData($path): ?array {
+        $jsonData = file_get_contents($path);
+        $arrayData = json_decode($jsonData, true, 512);
+        return $arrayData;
+    }
+
+    /**
+     * Array to Json.
+     * @param  array  $arrayData
+     * @return string $jsonData
+     */
+    public function setJsonData(array $arrayData) {
+        $encodedJson = json_encode($arrayData, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        $jsonData = mb_convert_encoding($encodedJson, "UTF-8");
+        return $jsonData;
+    }
+
+}
---
+++ src/Paginatior/PageOrganizer.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace SfUtils\Paginatior;
+
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
+/**
+ * Description of PageOrganizer
+ *
+ * @author passpos <paiap@outlook.com>
+ */
+class PageOrganizer {
+
+    private static Paginator $paginator;
+    private static int $pagination;
+    private static array $pieces;
+    private static int $current;
+    private static int $per;
+
+    public function paginate(Paginator $doctrinePaginator, int $currentPage, int $avg) {
+        if (self::$paginator == null) {
+            self::$paginator = $doctrinePaginator;
+            self::$current = $currentPage - 1;
+            self::$per = $avg;
+            self::setPagination();
+            self::setPieces();
+        }
+    }
+
+    /**
+     * 返回总页数
+     * return num of pages.
+     * 
+     * @param Paginator $paginator
+     * @param int $avg
+     * @return int 
+     */
+    public static function getPagination() {
+        return self::$pagination;
+    }
+
+    private static function setPagination(): void {
+        $max = self::$paginator->count();
+        self::$pagination = $max / self::$per;
+    }
+
+    static function getPieces(): array {
+        return self::$pieces;
+    }
+
+    /**
+     * 对查询结果进行分页。
+     * 
+     * @param Paginator $paginator
+     * @param int $per
+     * @return array 返回分页后的数组数据；
+     */
+    private static function setPieces() {
+        $all = iterator_to_array(self::$paginator);
+        self::$pieces = array_chunk($all, self::$per);
+    }
+
+    /**
+     * 返回一页（当前页）数据
+     * 
+     * @param int $current
+     * @return array
+     */
+    private static function setCurrentPage() {
+        if ((self::current >= 0) && (self::$current < (self::$pagination - 1))) {
+            return self::$pieces[$current];
+        } else {
+            return self::$pieces[0];
+        }
+    }
+
+    /**
+     * 返回一页（当前页）数据
+     * @param int $currentPage
+     * @return array
+     */
+    public static function getCurrentPage(int $currentPage) {
+        if (($currentPage >= 1) < ($currentPage < self::$pagination)) {
+            return self::$pieces[$currentPage];
+        } else {
+            return self::$pieces[0];
+        }
+    }
+
+}
